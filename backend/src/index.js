ad168553a9f9215a77f72e3c5d8b11bae9602179
Anthony E. <e4nthony@gmail.com>
2025-03-08 06:51:04 +0200

    back - added two paths in routing. front - added about and home pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+var path = require('path');
 
 const app = express()
 const port = 4422
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const page = path.join(__dirname, '../../frontend/home.html')
+  res.sendFile(page);
 })
+
+app.get('/home', (req, res) => {
+  const page = path.join(__dirname, '../../frontend/home.html')
+  res.sendFile(page);
+})
+
+app.get('/about', (req, res) => {
+  const page = path.join(__dirname, '../../frontend/about.html')
+  res.sendFile(page);
+});
 
 app.listen(port, () => {
   console.log(`Server running http://localhost:${port}/`)
