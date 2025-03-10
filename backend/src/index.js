9c4ead268b4d9335c7fd4c949398bd45eed68130
Anthony E. <e4nthony@gmail.com>
2025-03-10 07:32:52 +0200

    added ejs - to insert footer, header on server side into sended html. renamed html files to ejs. renamed pages folder to views.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,19 +4,23 @@ var path = require('path');
 const app = express()
 const port = 4422
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// -- Routes --
 app.get('/', (req, res) => {
-  const page = path.join(__dirname, '../../frontend/pages/home.html')
-  res.sendFile(page);
+  const page = path.join(__dirname, '../../frontend/views/home.ejs')
+  res.render(page);
 })
 
 app.get('/home', (req, res) => {
-  const page = path.join(__dirname, '../../frontend/pages/home.html')
-  res.sendFile(page);
+  const page = path.join(__dirname, '../../frontend/views/home.ejs')
+  res.render(page);
 })
 
 app.get('/about', (req, res) => {
-  const page = path.join(__dirname, '../../frontend/pages/about.html')
-  res.sendFile(page);
+  const page = path.join(__dirname, '../../frontend/views/about.ejs')
+  res.render(page);
 });
 
 app.listen(port, () => {
