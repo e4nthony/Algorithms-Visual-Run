2a8a74f0bd68f82490fea3c645ade1b85e2e6a96
Anthony E. <e4nthony@gmail.com>
2025-03-10 09:31:43 +0200

    added dynamic assemply of content depending on passed variable via url

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,34 @@ app.get('/about', (req, res) => {
   res.render(page);
 });
 
+app.get('/dynamic_page/', (req, res) => {
+  const page = path.join(__dirname, '../../frontend/views/dynamic_page.ejs')
+
+  res.render(page, { content_path: 'partials/default_dynamic_content.ejs' });
+});
+
+app.get('/dynamic_page/:initialContentType', (req, res) => {
+  const initialContentType = req.params.initialContentType; // Access the route parameter :initialContentType - variable that holds any parameter passed in url, and requires one to route here
+
+  const page = path.join(__dirname, '../../frontend/views/dynamic_page.ejs')
+
+  let contentFilePath; // content's file path for initial assembly of a page for client
+  switch (initialContentType) {
+    case '1':
+      contentFilePath = 'partials/default_dynamic_content.ejs';
+      break;
+    case '2':
+      contentFilePath = 'partials/default_dynamic_content.ejs';
+      break;
+    default:
+      contentFilePath = 'partials/default_dynamic_content.ejs';
+      break;
+  }
+
+  res.render(page, { content_path: contentFilePath });
+});
+
+
 app.listen(port, () => {
   console.log(`Server running http://localhost:${port}/`)
 })
